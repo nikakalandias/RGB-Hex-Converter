66c84d7020cb9f5e4b1a8b23a6a441e87165683d
nika <nikakalandias@gmail.com>
2021-12-25 15:34:51 +0300

    RGB-HEX Convert Function

--- RGB-Hex-Converter/ViewController.swift
+++ RGB-Hex-Converter/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblResult: UILabel!
     @IBOutlet weak var convertButton: UIButton!
     @IBOutlet weak var lblSelected: UILabel!
+    var secilenIndex : Int = 0
     
     
     
@@ -25,9 +26,60 @@ class ViewController: UIViewController {
     }
 
     @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        
+        secilenIndex = sender.selectedSegmentIndex
+        txtRed.text?.removeAll()
+        txtGreen.text?.removeAll()
+        txtBlue.text?.removeAll()
+        lblResult.text?.removeAll()
+        lblSelected.backgroundColor = UIColor.clear
+        
+        if secilenIndex == 0 {
+            
+        }
+        
+        else {
+            
+        }
+        
     }
     
     @IBAction func convertClickedButton(_ sender: Any) {
+        
+        if secilenIndex == 0 {
+            
+            convertRGBtoHEX()
+            
+        }else {
+            
+            convertHEXtoRGB()
+
+        }
+    }
+    
+    
+    
+    func convertRGBtoHEX() {
+        
+        if let redValue = UInt8(txtRed.text!) {
+            if let greenValue = UInt8(txtGreen.text!) {
+                if let blueValue = UInt8(txtBlue.text!) {
+                    
+                    let redHex = String(format: "%2X", redValue)
+                    let greenHex = String(format: "%2X", greenValue)
+                    let blueHex = String(format: "%2X", blueValue)
+                    
+                    lblResult.text = "HEX Color : #\(redHex) \(greenHex) \(blueHex)"
+                    lblSelected.backgroundColor = UIColor(red: CGFloat(redValue)/255, green: CGFloat(greenValue)/255, blue: CGFloat(blueValue)/255, alpha: 1)
+                    
+                }
+            }
+        }
+        
+    }
+    
+    func convertHEXtoRGB() {
+        
     }
     
     
