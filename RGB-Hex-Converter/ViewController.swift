91e4a4448dedf510a3fa89f40a475a8b909aa3ff
nika <nikakalandias@gmail.com>
2021-12-25 13:02:57 +0300

    Storyboard Design

--- RGB-Hex-Converter/ViewController.swift
+++ RGB-Hex-Converter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtRed: UITextField!
+    @IBOutlet weak var txtGreen: UITextField!
+    @IBOutlet weak var txtBlue: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var lblSelected: UILabel!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func convertClickedButton(_ sender: Any) {
+    }
+    
+    
 }
 
